e8aeecd89e0fb050edc9c870ee4c040b134739c3
Roman <36732258+RomaRomash@users.noreply.github.com>
2018-03-12 00:38:40 +0300

    Update bank.py

--- laba1/bank.py
+++ laba1/bank.py
@@ -1,5 +1,5 @@
 import account
-import converter
+import converte060318
 
 def main():
     rate = int(input("Введите процентную ставку: "))
@@ -7,9 +7,9 @@ def main():
     period = int(input("Введите период ведения счета в месяцах: "))
 
     result = account.calculate_income(rate, money, period)
-    for i in range(len(converter.v)):
-        a = converter.convert(money, i)
-        b = converter.convert(result, i)
+    for i in range(len(converte060318.v)):
+        a = converte060318.convert(money, i)
+        b = converte060318.convert(result, i)
         print("Параметры счета ({0}):\n".format(a[0]), "Сумма ({0}): {1}".format(a[0], a[1]), "\n", "Ставка: ", rate,
               "\n", "Период: ", period, "\n", "Сумма на счете в конце периода ({0}): {1}".format(b[0], b[1]))
         
